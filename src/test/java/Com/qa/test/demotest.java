a65ca84561424c0cbf715db9f0d97f6310e6d199
Ankita <ankitarai537@gmail.com>
2021-02-02 12:57:00 +0530

    again practice

---
+++ src/test/java/Com/qa/test/demotest.java
@@ -0,0 +1,27 @@
+package Com.qa.test;
+
+import org.testng.annotations.Test;
+
+public class demotest {
+	
+	@Test
+	public void sum() {
+		System.out.println("sum");
+	}
+	
+	@Test
+	public void sub() {
+		System.out.println("sub");
+	}
+	
+	@Test
+	public void mul() {
+		System.out.println("mul");
+	}
+	
+	@Test
+	public void div() {
+		System.out.println("div");
+	}
+
+}
